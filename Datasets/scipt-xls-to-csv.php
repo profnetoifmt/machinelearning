d92492cce38a86bc378f5af62aa98f621f190ad9
profnetoifmt <68514463+profnetoifmt@users.noreply.github.com>
2020-07-19 17:17:53 -0400

    Add files via upload

---
+++ Datasets/scipt-xls-to-csv.php
@@ -0,0 +1,20 @@
+<?php
+// converter xls em csv
+require_once('Classes/PHPExcel.php');
+ 
+//Usage:
+convertXLStoCSV('input.xls','output.csv');
+ 
+function convertXLStoCSV($infile,$outfile)
+{
+    $fileType = PHPExcel_IOFactory::identify($infile);
+    $objReader = PHPExcel_IOFactory::createReader($fileType);
+ 
+    $objReader->setReadDataOnly(true);   
+    $objPHPExcel = $objReader->load($infile);    
+ 
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'CSV');
+    $objWriter->save($outfile);
+}
+ 
+?>
